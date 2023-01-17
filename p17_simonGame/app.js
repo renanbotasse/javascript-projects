d97b6a60ef841af65c898cf8a895b05d67310067
renanbotasse <renanbotasse@gmail.com>
2023-01-17 19:20:22 +0000

    20 games

--- p17_simonGame/app.js
+++ p17_simonGame/app.js
@@ -190,7 +190,7 @@ function check() {
   if (playerOrder[playerOrder.length - 1] !== order[playerOrder.length - 1])
     good = false;
 
-  if (playerOrder.length == 3 && good) {
+  if (playerOrder.length == 20 && good) {
     winGame();
   }
 
