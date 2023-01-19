7aa0fe5240fb56ee4eb4011cccd85c5de73df130
renanbotasse <renanbotasse@gmail.com>
2023-01-19 12:52:47 +0000

    Done. Everything possible was customised.

--- p6_modal/app.js
+++ p6_modal/app.js
@@ -1,13 +1,15 @@
 const modalBtn = document.querySelector(".modal-btn");
 const modal = document.querySelector(".modal-overlay");
-//o modal sempre tá lá, nós que colocamos no CSS ele sem aparecer e usamos o JS para aparecer ou não
+
 const closeBtn = document.querySelector(".close-btn");
 
 modalBtn.addEventListener("click", function () {
   modal.classList.add("open-modal");
-  //muda a classe e torna visivel
 });
+
 closeBtn.addEventListener("click", function () {
   modal.classList.remove("open-modal");
-  //muda a classe e apaga
 });
+
+var videoElement = document.getElementById('videoId');    
+videoElement.webkitRequestFullScreen();
