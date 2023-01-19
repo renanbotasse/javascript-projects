87652214ef62dc2cbaafca51b8de44650def5bb0
renanbotasse <renanbotasse@gmail.com>
2023-01-19 23:35:03 +0000

    p10 2.0

--- p10_scroll/app.js
+++ p10_scroll/app.js
@@ -13,14 +13,12 @@ navToggle.addEventListener("click", function () {
 	} else {
 		linksContainer.style.height = 0;
 	}
-
-	});
-
+});
 
 const navbar = document.getElementById("nav");
 const topLink = document.querySelector(".top-link");
 	
-	window.addEventListener("scroll", function () {
+window.addEventListener("scroll", function () {
 	  const scrollHeight = window.pageYOffset;
 	  const navHeight = navbar.getBoundingClientRect().height;
 	  if (scrollHeight > navHeight) {
@@ -28,45 +26,34 @@ const topLink = document.querySelector(".top-link");
 	  } else {
 		navbar.classList.remove("fixed-nav");
 	  }
-
-	
 	  if (scrollHeight > 500) {
 		console.log("helo");
-	
 		topLink.classList.add("show-link");
 	  } else {
 		topLink.classList.remove("show-link");
 	  }
-	});
-
+});
 
 const scrollLinks = document.querySelectorAll(".scroll-link");
-
 scrollLinks.forEach((link) => {
   link.addEventListener("click", (e) => {
-
     e.preventDefault();
-
 	const id = e.currentTarget.getAttribute("href").slice(1);
     const element = document.getElementById(id);
-
     const navHeight = navbar.getBoundingClientRect().height;
     const containerHeight = linksContainer.getBoundingClientRect().height;
     const fixedNav = navbar.classList.contains("fixed-nav");
     let position = element.offsetTop - navHeight;
-
     if (!fixedNav) {
       position = position - navHeight;
     }
     if (navHeight > 82) {
       position = position + containerHeight;
     }
-
     window.scrollTo({
       left: 0,
       top: position,
     });
-
     linksContainer.style.height = 0;
   });
 });
