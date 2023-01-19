96d3dda86ff21b9775e097ddb53325ca685c5156
renanbotasse <renanbotasse@gmail.com>
2023-01-19 11:39:34 +0000

    Done. Everything possible was customised.

--- p4_navbar/app.js
+++ p4_navbar/app.js
@@ -2,6 +2,5 @@ const navToggle = document.querySelector(".nav-toggle");
 const links = document.querySelector(".links");
 
 navToggle.addEventListener('click', function () {
-	//quando clica ele "aplica show-links" no CSS
 	links.classList.toggle("show-links");
 });
